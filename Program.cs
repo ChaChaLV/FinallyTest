89038d4235c455488afcfcabdb20cc0ffcc333f8
ChaChaLV <ver4ik11@gmail.com>
2024-02-17 15:51:43 +0300

    Добавила описание решения в файле Readme и исправила знак в выводе массива

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿string [] startArray = new string [] {"123", "123456", "yes", "fun", "sunny"};
+﻿string [] startArray = new string [5] {"123", "123456", "yes", "fun", "sunny"};
 string []finalArray = new string [startArray.Length];
-int count = 0;
+
 
 void GetResult (string [] startArray, string []finalArray)
 {
+    int count = 0;
     for (int i = 0; i < startArray.Length; i++)
     {
         if (startArray[i].Length <= 3)
@@ -17,6 +18,9 @@ void PrintResult (string [] array)
 {
     for (int i = 0; i<array.Length; i++)
     {
-        Console.Write (array[i] = " ");
+        Console.Write (array[i] + " ");
     }
 }
+
+GetResult (startArray,finalArray);
+PrintResult (finalArray);
