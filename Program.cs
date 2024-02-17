11e52420df09387515d52509af6960f86c6a8f4c
ChaChaLV <ver4ik11@gmail.com>
2024-02-17 15:24:11 +0300

    Задала массив, проверяю строки на условие и записываю в новый массив

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿string [] startArray = new string [] {"123", "123456", "yes", "fun", "sunny"};
+string []finalArray = new string [startArray.Length];
+int count = 0;
+
+for (int i = 0; i < startArray.Length; i++)
+{
+    if (startArray[i].Length <= 3)
+    {
+        finalArray[count] = startArray[i];
+        count++;
+    }
+}
